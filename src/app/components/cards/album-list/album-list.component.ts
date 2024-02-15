d6a2e809275a64b35b7ccbb5820f3d141925eed8
Lucas Napomucena <lucas.nepomucena.ext@spms.min-saude.pt>
2024-02-15 10:13:41 +0000

    feat: add structure

---
+++ src/app/components/cards/album-list/album-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-album-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './album-list.component.html',
+  styleUrl: './album-list.component.scss'
+})
+export class AlbumListComponent {
+
+}
