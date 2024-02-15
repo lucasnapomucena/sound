ad8b2d80c6d87b362b480a1def963378d722dafb
Lucas Napomucena <lucas.nepomucena.ext@spms.min-saude.pt>
2024-02-15 10:43:40 +0000

    feat: add sidebar and main content

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from '@angular/router';
+import { AlbumComponent } from './pages/album/album.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: '', component: AlbumComponent }];
--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
+import { SidebarComponent } from './../sidebar/sidebar.component';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent, HeaderComponent],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss'
 })
-export class LayoutComponent {
-
-}
+export class LayoutComponent {}
